3f15a09479f810f7702f447b9eca6da405f63c24
John Doe <xevoxe@gmail.com>
2020-02-03 20:24:20 -0600

    EventQueue

--- src/server/game.js
+++ src/server/game.js
@@ -1,11 +1,15 @@
 const Server = require('./server');
-const EventQueue = require('../shared/eventQueue');
+const EventSystem = require('../shared/eventQueue');
+const EventScheduler = require('../shared/eventQueue');
 const Entity = require('../game/entity');
 const Position = require('../game/position');
 const Vector2D = require('../game/utils/vector2D');
 const Constants = require('./constants');
+const Events = require('../shared/events');
 
 const ECS = require( '../shared/ecs');
+
+const util = require('util');
 
 
 class Game {
@@ -13,51 +17,84 @@ class Game {
     #sockets = [];
     #server;
     #lastUpdate;
+    #simLag;
     #ecs = new ECS();
-    #eventQueue;
+    #eventQueue = new EventSystem.EventQueue();
+    #eventScheduler;
     
     constructor(){
         this.#server = new Server(1,3000,this);
-        this.#eventQueue = new EventQueue();
+        
+       
+        this.#eventScheduler = new EventSystem.EventScheduler(this.#eventQueue);
+
     }
           
 
     Init(){
         this.#server.Init();
         // this.#server = new Server(1,3000,this);
-        //Update every tick
-        setInterval(this.update.bind(this), Constants.GAME_TICK);
+
     }
 
   
 
     Start(){
         this.#server.Start();
-        this.#lastUpdate = Date.now();
 
+        this.#lastUpdate = Date.now();
+        this.#simLag = 0;
+      
+        let update = this.#eventScheduler.createEvent(Events.UpdateSimulation);
+        update.data.timeStamp = this.#lastUpdate;
+        this.#eventScheduler.schedule(update,Constants.GAME_TICK);
+        this.pollEventQueue();
     }
 
     update(){
+        console.log("UPDATE SIM");
         let currentTime = Date.now();
         let dt = currentTime - this.#lastUpdate;
         this.#lastUpdate = currentTime; 
+        this.#simLag = this.#simLag + dt - 600;
+        console.log(`DT= ${dt}`);
+        console.log(`Sim LAG = ${this.#simLag}`);
         //Get last update time
         //Get current time
         //elaspe time = currentTime - lastUpdateTime
         //Update Simulation 
         //Catch the server up to gameworld time by scheduling next update to be elapseTime - tickRate
         //Start polling event queue with the lastUpdateTime
-        this.pollEventQueue();
+ 
+        let update = this.#eventScheduler.createEvent(Events.UpdateSimulation);
+        update.data.timeStamp = this.#lastUpdate;
 
+        let timer = 600 - this.#simLag;
+
+        this.#eventScheduler.schedule(update, timer > 0 ? timer : 0);
     }
 
     pollEventQueue(){
         //Poll Event Queue until empty or until events = lastUpdateTime
         let event;
         do{
-            event = this.#eventQueue.poll();
-        }while(event);
+            event = this.#eventQueue.poll(this.#lastUpdate);
+            if(event){
+                if(event.name === "UPDATE_SIMULATION"){
+                    this.update();
+                }
+
+                if(event.name === "ADD_PLAYER_TO_WORLD"){
+
+                }
+            }
+
         
+            
+        }while(event); 
+
+        //Setup up polling to occur.
+        setTimeout(()=> this.pollEventQueue(),300);
     }
 
 
